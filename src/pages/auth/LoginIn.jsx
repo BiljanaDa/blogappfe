d194ce6fd675701a6688f93686905e510f08c005
miodrag78 <135058950+miodrag78@users.noreply.github.com>
2024-02-05 21:53:12 +0100

    login

---
+++ src/pages/auth/LoginIn.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { Alert, Container } from "react-bootstrap";
+ import Button from 'react-bootstrap/Button';
+        import Form from 'react-bootstrap/Form';
+import AuthService from "../../services/auth.service";
+
+const defaultData = {email: "",password:""}
+
+export default function Login() {
+    const [message, setMessage] = useState("")
+    const[formData, setFormData] = useState(defaultData)
+    const[errors, setErrors] = useState(defaultData)
+    async function onSubmit() {
+        // if(formData.title.length===0 || formData.body===0)
+        // return alert ("Title and body must be specified");
+        try{
+            setErrors(defaultData);
+            const response = await AuthService.login(formData);
+            console.log(response);
+            if(response) {
+                setFormData(defaultData);
+                setMessage("")
+            }
+        }
+        catch(e) {
+            const _errors = e?.response?.data?.errors;
+            if(_errors) {
+                const keys = Object.keys(errors);
+            // keys.forEach(key=> setErrors({...errors, key:errors[key]}))
+            setErrors({
+               
+                email: _errors?.email?.join(' ') || "",
+                password: _errors?.password?.join(' ') || "",
+                
+            })
+            }
+            
+        }
+        
+    }
+    return (
+       <Container style={{ backgroundColor:"grey", padding:"1rem", marginTop:"10rem" }}>
+        <Form>
+        
+  
+        <Form.Group className="mb-3" >
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Email"  value={formData.email} onChange={(e)=>setFormData({...formData, email:e.target.value})}/>
+          {errors.email&& <Alert variant="danger">{errors.email}</Alert>} 
+        </Form.Group>
+        <Form.Group className="mb-3" >
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password"  value={formData.password} onChange={(e)=>setFormData({...formData, password:e.target.value})}/>
+          {errors.password&& <Alert variant="danger">{errors.password}</Alert>} 
+        </Form.Group>
+       
+     
+        {message && (
+            <Alert variant="success" >{message}</Alert>
+        )}
+        <Button variant="primary" type="button" onClick={onSubmit}>
+          Register
+        </Button>
+      </Form>
+      </Container>
+    )
+}
--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router";
+import Login from "./pages/auth/LoginIn";
 import Register from "./pages/auth/Register";
 import CreatePost from "./pages/CreatePost";
 import PostPage from "./pages/PostPage";
@@ -12,6 +13,7 @@ export default function router() {
       <Route path="/posts/:id" element={<PostPage />} />
       <Route path ='/createpost' element= {<CreatePost />} />
       <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
     </Routes>
   );
 }
